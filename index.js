2a21480dee0ec12437478a9627b9720e4f5a315d
datsal <58320887+realdatsal@users.noreply.github.com>
2025-09-04 12:46:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
-
 export default {
   async fetch(request, env, ctx) {
-    try {
-      return await getAssetFromKV(
-        { request, waitUntil: ctx.waitUntil },
-        env
-      )
-    } catch (err) {
-      return new Response("Not Found", { status: 404 })
-    }
+    return env.ASSETS.fetch(request)
   }
 }
